344ddb0990ca2ff2bb031f40823c793180755429
AleksandrsLoputevs <aleksandrloputevs@gmail.com>
2021-08-31 16:54:23 +0100

    3.3. Разбор первой программы. [#159033 #86408]

---
+++ src/Alik.java
@@ -0,0 +1,11 @@
+public class Alik {
+    public static void main(String[] args) {
+        System.out.println("I'm Alik");
+        System.out.println("1 + 3 = 4");
+        System.out.println("1 + 8 = 9" +
+                ""
+        );
+        System.out.println("2 * 8 = 16");
+
+    }
+}
