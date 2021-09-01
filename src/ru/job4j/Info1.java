d96daf22246513a8d7bae1178df1cd61724ec16b
AleksandrsLoputevs <aleksandrloputevs@gmail.com>
2021-09-01 16:35:08 +0100

    3.4. Заголовок в файле java [#159036 #86407]

---
+++ src/ru/job4j/Info1.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Info1 {
+    public static void main(String[] args) {
+        System.out.println("I love you with 01 09 21");
+        System.out.println("I well by u4itsja");
+    }
+}
