a389a75d705c2bbe932644638a97d1fa6402cd3a
AleksandrsLoputevs <aleksandrloputevs@gmail.com>
2021-09-03 17:57:01 +0100

    3.4. Заголовок в файле java [#159036 #86407]

---
+++ src/ru/job4j/Alik2.java
@@ -0,0 +1,19 @@
+package ru.job4j;
+
+public class Alik2 {
+    public static void main(String[] args) {
+        byte man = 1;
+        int vasja = 5;
+        String mama = "vasja";
+        boolean sasa = true;
+        double van = 10.5;
+        System.out.println(man);
+        System.out.println(vasja);
+        System.out.println(mama);
+        System.out.println(sasa);
+        System.out.println(van);
+
+
+
+    }
+}
