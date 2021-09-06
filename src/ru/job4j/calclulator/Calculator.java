4f3ff1e38dcd6d520eed397cba7dee495cc275fa
aleks <aleksandrloputevs@gmail>
2021-09-06 16:28:51 +0100

    3. Арифметические операции.3.1. Переназначение переменной [#232354]

---
+++ src/ru/job4j/calclulator/Calculator.java
@@ -0,0 +1,11 @@
+package ru.job4j.calclulator;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int size =10;
+        size = size + 100;
+        size = size /5;
+        System.out.println(size / 5);
+
+    }
+}
