bf2a7e61b5d67958359af33fcfa10a50d72d3af0
aleks <aleksandrloputevs@gmail>
2021-09-07 17:21:01 +0100

    metod plus

--- src/ru/job4j/calclulator/Calculator.java
+++ src/ru/job4j/calclulator/Calculator.java
@@ -1,11 +1,32 @@
 package ru.job4j.calclulator;
 
+import java.util.concurrent.Callable;
+
 public class Calculator {
+    public static void plus(int first, int second) {
+        int result = first + second;
+        System.out.println(result);
+    }
+
     public static void main(String[] args) {
-        int size =10;
+        int size = 10;
         size = size + 100;
-        size = size /5;
+        size = size / 5;
         System.out.println(size / 5);
+        Calculator.plus(1, 2);
+        Calculator.plus(10, 11);
+        Calculator.plus(100,400);
+        Calculator.plus(4,9);
+        Calculator.plus (60,90);
+
 
     }
-}
+};
+
+
+
+
+
+
+
+
