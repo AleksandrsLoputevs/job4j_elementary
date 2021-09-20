7fcbdf80bdd358a8c97ad96d380bd0a56396e2b3
aleks <aleksandrloputevs@gmail>
2021-09-20 15:53:33 +0100

    7. Расстояние между точками в системе координат

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,16 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String papa = "mama moet ramu";
+        System.out.println("papa");
+        String name ="Danja moj syn, emu";
+        byte age = 21;
+        String separate = " - ";
+        String surname ="ja ochenj rad .";
+        String id = name + age + separate + surname + "years old";
+
+        System .out .println(id);
+        
+    }
+}
