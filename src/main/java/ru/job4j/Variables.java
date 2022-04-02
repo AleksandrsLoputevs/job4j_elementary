44e087fe78ad7bc8d4885af0ac7f950072949ce6
aleks <aleksandrloputevs@gmail>
2022-04-02 20:02:37 +0100

    4.2. Результат работы метода [#232622]

---
+++ src/main/java/ru/job4j/Variables.java
@@ -0,0 +1,15 @@
+package ru.job4j;
+
+public class Variables {
+    public static void main(String[] args) {
+        int vozrast = 55 ;
+        boolean men = true ;
+        int rost = 189 ;
+        double kirpich = 1985.5 ;
+        double bakteri = 138.5 ;
+        int luna = 200867 ;
+        boolean razreshenieNaRabotu = true;
+        int fail = 61000 ;
+        String love = "love is love";
+    }
+}
