b2b283f53d102801950fd01d28388cd818a47870
aleks <aleksandrloputevs@gmail>
2022-04-03 14:17:37 +0100

    8. Стороны прямоугольника. [#137735 #86435]

---
+++ src/main/java/ru/job4j/condition/Area.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class Area {
+    public static double square(double height, double width) {
+    double rsl = (height * width);
+    return rsl;
+}
+
+    public static void main(String[] args) {
+        double result1 = Area.square(4, 1);
+        System.out.println(" height 4 * width 1 real = " + result1);
+    }
+}
